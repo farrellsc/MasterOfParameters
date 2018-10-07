8dbbcd50b269d7d0880e5a8f2d85ae792905cc12
farrellsc <farrellsc@163.com>
2018-10-07 14:40:12 -0400

    gg

--- scripts/word2vec_script.py
+++ scripts/word2vec_script.py
@@ -6,10 +6,9 @@ import numpy as np
 
 database = "/root/MasterOfParameters/data/"
 predpath = "/root/MasterOfParameters/predictions/GRU2_mini.csv"
-# wvSize = 100
-# myDataLoader = dataLoader(database + 'train.csv', database + 'test.csv', 512, 6)
+wvSize = 100
+myDataLoader = dataLoader(database + 'train.csv', database + 'test.csv', 512, 6)
 
-# myModel = DCRNN(myDataLoader, predpath, 5)
-myModel = DCRNN(None, predpath, 5)
-# myModel.train()
-# myModel.predict()
+myModel = DCRNN(myDataLoader, predpath, 5)
+myModel.train()
+myModel.predict()
