9ec994004e540b32803a4a345eeb241102251e22
farrellsc <farrellsc@163.com>
2018-10-07 14:06:10 -0400

    minor

--- scripts/word2vec_script.py
+++ scripts/word2vec_script.py
@@ -9,6 +9,7 @@ predpath = "/root/MasterOfParameters/predictions/GRU2_mini.csv"
 wvSize = 100
 myDataLoader = dataLoader(database + 'train.csv', database + 'test.csv', 512, 6)
 
+print("---------------------------loading data----------------------------------")
 train_input_data_str = [[str(one) for one in two] for two in myDataLoader.train_input_data]
 test_input_data_str = [[str(one) for one in two] for two in myDataLoader.test_input_data]
 wvModel = Word2Vec(train_input_data_str, size=wvSize, window=5, min_count=1, workers=4)
@@ -27,6 +28,8 @@ myDataLoader.train_input_data = new_train_input
 myDataLoader.test_input_data = new_test_input
 myDataLoader.split_data()
 
+
+print("---------------------------training----------------------------------")
 myModel = DCRNN(myDataLoader, predpath, 5)
 myModel.train()
 myModel.predict()
